efbc4adfbb874c104e9fee481869c2594633ab41
CandidoRibeiro <candido.peralva@avantsoft.com.br>
2024-08-03 19:11:52 -0300

    start conect historic api

--- src/api/histApi.js
+++ src/api/histApi.js
@@ -1,43 +1,14 @@
 // src/api/histApi.js
 const HIST_API_URL = 'http://localhost:3000'; // Mude para o URL de deploy quando necessário
+const API_TOKEN = ''; // Mude para o URL de deploy quando necessário
 
-export const getHistData = async (endpoint) => {
-    try {
-        const response = await fetch(`${HIST_API_URL}/${endpoint}`, {
-            method: 'GET',
+const callAPI = async (method, body) => {
+    return await fetch(`${HIST_API_URL}/${endpoint}`, {
+            method: method,
             headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error('Erro na resposta da API local');
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Erro ao buscar dados da API local:', error);
-        throw error;
-    }
-};
-
-export const postHistData = async (endpoint, data) => {
-    try {
-        const response = await fetch(`${HIST_API_URL}/${endpoint}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${API_TOKEN}`
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(body)
         });
-
-        if (!response.ok) {
-            throw new Error('Erro ao enviar dados para a API local');
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Erro ao enviar dados para a API local:', error);
-        throw error;
-    }
 };
